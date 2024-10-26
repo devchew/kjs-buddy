f5199bd60c43b2ab5616d1eed61264d0cd9b59b3
DevChew <the@devchew.com>
2024-10-26 11:48:45 +0200

    pwa

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,47 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const useOffline = () => {
+    const [isOffline, setIsOffline] = useState(!navigator.onLine)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const goOnline = () => setIsOffline(false)
+    const goOffline = () => setIsOffline(true)
+
+    window.addEventListener('online', goOnline)
+    window.addEventListener('offline', goOffline)
+
+    return isOffline
+}
+
+function App () {
+    const [count, setCount] = useState(0)
+    const isOffline = useOffline()
+
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+            {isOffline && <p className="offline">You are offline</p>}
+        </>
+    )
 }
 
 export default App
