5c8c24b29c4ff539f299b023265659cc701ded2a
DevChew <the@devchew.com>
2024-10-27 12:00:37 +0100

    scale based on window width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import "./Header.css";
 export const Header: FunctionComponent = () => {
     return (
         <div className="header">
-            <img width={110} height={60} src={monte} className="header__eventLogo"/>
+            <img  src={monte} className="header__eventLogo"/>
             <div className="header__carNumber">
                 <div className="header__carNumber__title">Numer auta</div>
                 <div className="header__carNumber__number">00</div>
@@ -15,7 +15,7 @@ export const Header: FunctionComponent = () => {
                 <span className="header__eventName__title">Karta drogowa 1<br/></span>
                 <span className="header__eventName__name">Rally monte calvaria<br/>17.02.2024</span>
             </div>
-            <img width={60} height={60} src={pzm} className="header__eventSponsorLogo"/>
+            <img src={pzm} className="header__eventSponsorLogo"/>
         </div>
     )
 }
--- src/components/PanelDetails.tsx
+++ src/components/PanelDetails.tsx
@@ -12,17 +12,16 @@ export const PanelDetails: FunctionComponent<{starting?: boolean}> = ({starting}
     <div className="panelDetails">
         <span className="panelDetails__name">PS1 - Krosno</span>
         {!starting && <div className="panelDetails__finish">
-            <IconInRedCircle className="panelDetails__finish__icon"><PiFlagCheckeredFill fill="#000"
-                                                                                         size={22}/></IconInRedCircle>
+            <IconInRedCircle className="panelDetails__finish__icon"><PiFlagCheckeredFill fill="#000" /></IconInRedCircle>
             <TimePicker title="Czas Mety" value={6646460} precision="miliseconds"/>
         </div>}
         <div className="panelDetails__times">
             {!starting && <>
                 <TimePicker title="Prowizoryczny start" value={6646460} precision="minutes"/>
-                <PiCaretRightFill size={25} className="panelDetails__times__arrow"/>
+                <PiCaretRightFill className="panelDetails__times__arrow"/>
                 <TimePicker title="Faktyczny start" value={6646460} precision="minutes" style="gray"/>
-                <IconInRedCircle className="panelDetails__times__icon"><PiFlagFill fill="#000" size={22}/></IconInRedCircle>
-                <PiCaretRightFill size={25} className="panelDetails__times__arrow"/>
+                <IconInRedCircle className="panelDetails__times__icon"><PiFlagFill fill="#000"/></IconInRedCircle>
+                <PiCaretRightFill className="panelDetails__times__arrow"/>
             </>}
             <TimePicker title="Czas przejazdu" value={6646460} precision="minutes"/>
         </div>
