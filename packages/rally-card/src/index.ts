2b53f4030b6da60fc25994918b3c131a1b8ff577
DevChew <the@devchew.com>
2025-05-10 15:26:53 +0200

    move card to external lib

---
+++ packages/rally-card/src/index.ts
@@ -0,0 +1,3 @@
+export { Card } from './components/Card';
+export {useCardContext, type CardContextType, CardProvider} from './contexts/CardContext';
+export {type PanelContextType, usePanelContext, PanelProvider} from './contexts/PanelContext'
--- packages/rally-card/src/main.d.ts
+++
@@ -1,3 +0,0 @@
-export { Card } from './components/Card';
-export * from './contexts/CardContext';
-export * from './contexts/PanelContext';
