135ced8c86cee236a360e725e8199bec2408ccb3
DevChew <the@devchew.com>
2025-05-17 14:04:25 +0200

    bump

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,34 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({
-    strategies: 'injectManifest',
-    srcDir: 'src',
-    filename: 'sw.ts',
-    registerType: 'autoUpdate',
-    injectRegister: 'inline',
+  //   VitePWA({
+  //   strategies: 'injectManifest',
+  //   srcDir: 'src',
+  //   filename: 'sw.ts',
+  //   registerType: 'autoUpdate',
+  //   injectRegister: 'inline',
 
-    pwaAssets: {
-      disabled: true,
-    },
+  //   pwaAssets: {
+  //     disabled: true,
+  //   },
 
-    manifest: false,
+  //   manifest: false,
 
-    injectManifest: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-    },
+  //   injectManifest: {
+  //     globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+  //   },
 
-    devOptions: {
-      enabled: true,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })
+  //   devOptions: {
+  //     enabled: true,
+  //     navigateFallback: 'index.html',
+  //     suppressWarnings: true,
+  //     type: 'module',
+  //   },
+  // })
 ],
   server:{
     port: 7777
